ad33d855c49d19169f0069e07ef35f8d79c9eb7f
Ubuntu <azureuser@vm1.yxbbss3ls53epcqkssjh0m53xb.xx.internal.cloudapp.net>
2024-11-16 20:37:24 +0000

    added

---
+++ vm/main.tf
@@ -0,0 +1,167 @@
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet1" {
+  name                = var.vneta
+  address_space       = ["10.5.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_virtual_network" "vnet2" {
+  name                = var.vnetb
+  address_space       = ["10.15.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Subnets
+resource "azurerm_subnet" "subnet1" {
+  name                 = var.subnet1
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = ["10.5.0.0/24"]
+}
+
+resource "azurerm_subnet" "subnet2" {
+  name                 = var.subnet2
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet2.name
+  address_prefixes     = ["10.15.0.0/24"]
+}
+
+# Network Peering
+resource "azurerm_virtual_network_peering" "peering1to2" {
+  name                      = "vnet1-to-vnet2"
+  resource_group_name       = azurerm_resource_group.rg.name
+  virtual_network_name      = azurerm_virtual_network.vnet1.name
+  remote_virtual_network_id = azurerm_virtual_network.vnet2.id
+  allow_forwarded_traffic   = true
+  allow_gateway_transit     = false
+  use_remote_gateways       = false
+}
+
+resource "azurerm_virtual_network_peering" "peering2to1" {
+  name                      = "vnet2-to-vnet1"
+  resource_group_name       = azurerm_resource_group.rg.name
+  virtual_network_name      = azurerm_virtual_network.vnet2.name
+  remote_virtual_network_id = azurerm_virtual_network.vnet1.id
+  allow_forwarded_traffic   = true
+  allow_gateway_transit     = false
+  use_remote_gateways       = false
+}
+
+# SSH Key for VMs
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+resource "local_file" "private_key" {
+  content  = tls_private_key.ssh_key.private_key_pem
+  filename = "${path.module}/id_rsa"
+}
+
+resource "local_file" "public_key" {
+  content  = tls_private_key.ssh_key.public_key_openssh
+  filename = "${path.module}/id_rsa.pub"
+}
+
+# VM1 in Network1
+resource "azurerm_network_interface" "vm1_nic" {
+  name                = "vm1-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet1.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm1_public_ip.id
+  }
+}
+
+resource "azurerm_public_ip" "vm1_public_ip" {
+  name                = "vm1-public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static" 
+}
+
+
+
+resource "azurerm_linux_virtual_machine" "vm1" {
+  name                  = "vm1"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  size                  = "Standard_B1s"
+  admin_username        = "adminuser"
+  network_interface_ids = [azurerm_network_interface.vm1_nic.id]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = tls_private_key.ssh_key.public_key_openssh
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "ubuntu-24_04-lts"
+    sku       = "server"
+    version   = "latest"
+  }
+}
+
+# VM2 in Network2
+resource "azurerm_network_interface" "vm2_nic" {
+  name                = "vm2-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet2.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm2" {
+  name                  = "vm2"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  size                  = "Standard_B1s" 
+  admin_username        = "adminuser"
+  network_interface_ids = [azurerm_network_interface.vm2_nic.id]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = tls_private_key.ssh_key.public_key_openssh
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "ubuntu-24_04-lts" 
+    sku       = "server"
+    version   = "latest"
+  }
+}
+
+output "vm1_public_ip" {
+  value = azurerm_public_ip.vm1_public_ip.ip_address
+}
+
+output "vm2_private_ip" {
+  value = azurerm_network_interface.vm2_nic.private_ip_address
+}
---
+++ vm/variable.tf
@@ -0,0 +1,24 @@
+variable "rg"{
+default = "rg-11"
+}
+
+variable "location"{
+default = "Australia East"
+}
+
+variable "vneta"{
+default = "vnet1"
+}
+
+variable "vnetb"{
+default = "vnet2"
+}
+
+variable "subnet1"{
+default = "subnet1"
+}
+
+
+variable "subnet2"{
+default = "subnet2"
+}
