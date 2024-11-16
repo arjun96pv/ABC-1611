941b1bab0f0586df843841fac8bf619d26493550
Ubuntu <azureuser@vm1.yxbbss3ls53epcqkssjh0m53xb.xx.internal.cloudapp.net>
2024-11-16 16:06:00 +0000

    added

---
+++ vnet2/variable.tf
@@ -0,0 +1,59 @@
+variable "rg_name" {
+  default = "ajterraform_rg"
+}
+
+variable "location" {
+  default = "East Asia"
+}
+
+variable "address_space" {
+  default = ["10.15.0.0/16"]
+  type    = list(string)
+}
+
+variable "vnet_name" {
+  default = "terraform_vnet"
+}
+variable "subnet_space" {
+  default = ["10.15.0.0/24", "10.15.10.0/24"]
+  type    = list(string)
+}
+
+variable "subnet_name" {
+  default = "terraform_snet"
+}
+
+variable "key" {
+  default = "ssh_key"
+}
+
+variable "nic_name" {
+  default = "nic"
+}
+variable "pip_name" {
+  default = "pip"
+}
+
+variable "ip_name" {
+  default = "ip"
+}
+variable "sku" {
+  default = "22.04-LTS"
+}
+variable "vm_name" {
+  default = "vm"
+}
+variable "admin" {
+  default = "azureuser"
+}
+
+variable "size" {
+  default = "Standard_DS1_v2"
+}
+variable "user" {
+  default = "azureuser"
+}
+variable "nsg" {
+  default = "nsg"
+}
+
